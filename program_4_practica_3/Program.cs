9dbf607376630c08b98b616862a86cf35a0d6038
Danny <dannyconcepcion01@gmail.com>
2020-09-13 23:42:43 -0400

    Agregar archivos de proyecto.

---
+++ program_4_practica_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_4_practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+                    
+        {
+            doblar();
+
+        }
+         static void   doblar()
+        {
+            return ;
+
+            int numero;
+            int num;
+            int resultado;
+            numero = num;
+
+                       
+
+            Console.WriteLine("Digite un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            resultado = numero * num;
+
+
+
+            Console.WriteLine("El resultado es "+ resultado);
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+       
+      
+}
